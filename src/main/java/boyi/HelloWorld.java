e735b4b78d5d0781996f15c1b7b36d226991856e
zhangboyi <15813380401@163.com>
2020-09-29 11:20:37 +0800

    unit

--- src/main/java/boyi/HelloWorld.java
+++ src/main/java/boyi/HelloWorld.java
@@ -1,16 +1,6 @@
 package boyi;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-
-        printNum(3);
-        printNum(0);
-        printStr("hello");
-        printStr("world");
-
-
-    }
-
 
     public static int printNum(int i){
         if(i==3){
--- src/test/java/testHelloWorld.java
+++ src/test/java/testHelloWorld.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static boyi.HelloWorld.printNum;
+import static boyi.HelloWorld.printStr;
 
 public class testHelloWorld {
 
@@ -12,5 +13,9 @@ public class testHelloWorld {
     public void testhello2(){
         assert 0==printNum(2);
 
+    }
+    @Test
+    public void testhello3(){
+        assert "hello".equals(printStr("hello"));
     }
 }
