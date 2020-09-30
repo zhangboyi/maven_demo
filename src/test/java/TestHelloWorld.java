efc3f7c83f28c030d40e04be9840957ea3e22edc
zhangboyi <15813380401@163.com>
2020-09-30 12:24:10 +0800

    update

--- src/test/java/TestHelloWorld.java
+++ src/test/java/TestHelloWorld.java
@@ -1,21 +1,24 @@
-import junit.framework.TestCase;
 
 import static boyi.HelloWorld.printNum;
 import static boyi.HelloWorld.printStr;
+import static org.junit.Assert.assertTrue;
 
-public class TestHelloWorld extends TestCase {
+import org.springframework.util.Assert;
+import org.junit.Test;
 
+public class TestHelloWorld {
 
+    @Test
     public void testHello1(){
         assertTrue(3==printNum(3)) ;
 
     }
-
+    @Test
     public void testHello2H(){
         assert 0==printNum(2);
 
     }
-
+    @Test
     public void testHello3(){
         assert "hello".equals(printStr("hello"));
     }
