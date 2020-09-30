dddd3d2ea0d52036ddb47811c993a15ee489f9b2
zhangboyi <15813380401@163.com>
2020-09-30 10:27:05 +0800

    更改build.xml

--- src/main/java/boyi/TestHelloWorld.java
+++ src/main/java/boyi/TestHelloWorld.java
@@ -1,24 +1,23 @@
 package boyi;
 
 import junit.framework.TestCase;
-import org.junit.jupiter.api.Test;
+
 import static boyi.HelloWorld.printNum;
 import static boyi.HelloWorld.printStr;
-import static org.junit.Assert.assertTrue;
 
 public class TestHelloWorld extends TestCase {
 
-    @Test
+
     public void testHello1(){
         assertTrue(3==printNum(3)) ;
 
     }
-    @Test
+
     public void testHello2H(){
         assert 0==printNum(2);
 
     }
-    @Test
+
     public void testHello3(){
         assert "hello".equals(printStr("hello"));
     }
