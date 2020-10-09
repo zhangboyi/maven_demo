c2eb0ce125150e14815ce38422e68018ad7269ee
zhangboyi <15813380401@163.com>
2020-10-09 15:51:41 +0800

    update

--- src/main/java/com/example/boyi/HelloWorld.java
+++ src/main/java/com/example/boyi/HelloWorld.java
@@ -2,14 +2,14 @@ package com.example.boyi;
 
 public class HelloWorld {
 
-    public static int printNum(int i){
+    public int printNum(int i){
         if(i==3){
             return 3;
         }
         return 0;
 
     }
-    public static String printStr(String str){
+    public String printStr(String str){
         if (str.equals("hello")){
             return "hello";
         }else{
--- src/main/java/com/test/boyi/HelloWorldTest.java
+++ src/main/java/com/test/boyi/HelloWorldTest.java
@@ -1,25 +1,25 @@
 package com.test.boyi;
 
-import static com.example.boyi.HelloWorld.printNum;
-import static com.example.boyi.HelloWorld.printStr;
+import com.example.boyi.HelloWorld;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class HelloWorldTest {
+    HelloWorld helloWorld = new HelloWorld();
 
     @Test
     public void testHello1(){
-        Assert.assertTrue(3==printNum(3)) ;
+        Assert.assertTrue(3==helloWorld.printNum(3)) ;
 
     }
     @Test
     public void testHello2H(){
-        assert 0==printNum(2);
+        assert 0==helloWorld.printNum(2);
 
     }
     @Test
     public void testHello3(){
-        assert "hello".equals(printStr("hello"));
+        assert "hello".equals(helloWorld.printStr("hello"));
     }
 }
